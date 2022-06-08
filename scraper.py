d16da8c5a4b0c877abae7d000fb69a7c42eeb99b
Joe <jgasko@gmail.com>
2022-06-08 16:20:40 -0400

    added loop to coninuously change to random wikipedia page titles.

--- scraper.py
+++ scraper.py
@@ -1,13 +1,30 @@
-from urllib import response
 import requests
 from bs4 import BeautifulSoup
+import random
 
-response = requests.get(
-    url="https://en.wikipedia.org/wiki/Web_scraping",
-)
-print(response.status_code)
+def scrapeWikiArticle(url):
+	response = requests.get(
+		url=url,
+	)
+	
+	soup = BeautifulSoup(response.content, 'html.parser')
 
-soup = BeautifulSoup(response.content, 'html.parser')
+	title = soup.find(id="firstHeading")
+	print(title.text)
 
-title = soup.find(id="firstHeading")
-print(title.string)
+	allLinks = soup.find(id="bodyContent").find_all("a")
+	random.shuffle(allLinks)
+	linkToScrape = 0
+
+	for link in allLinks:
+		# We are only interested in other wiki articles
+		if link['href'].find("/wiki/") == -1: 
+			continue
+
+		# Use this link to scrape
+		linkToScrape = link
+		break
+
+	scrapeWikiArticle("https://en.wikipedia.org" + linkToScrape['href'])
+
+scrapeWikiArticle("https://en.wikipedia.org/wiki/List_of_members_elected_to_the_General_National_Congress,_2012")
