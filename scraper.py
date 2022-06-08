74d73148c6834e6b84e5bdbfd29b4d2e6743aa71
Joe <jgasko@gmail.com>
2022-06-08 15:09:55 -0400

    initial commit

---
+++ scraper.py
@@ -0,0 +1,13 @@
+from urllib import response
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(
+    url="https://en.wikipedia.org/wiki/Web_scraping",
+)
+print(response.status_code)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+title = soup.find(id="firstHeading")
+print(title.string)
